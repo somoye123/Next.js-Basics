27b5a6d64a9cdb553163f9506ffe75b532814129
somoye123 <somoye.ayotunde@gmail.com>
2020-12-15 16:42:43 +0100

    import Link from next/link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-
+import Head from "next/head";
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="container">
@@ -54,7 +54,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +205,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
